acb77f441ecd76fabb236c737f91a89c6ffe8750
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-08-07 21:58:32 +0530

    front pages and browser routing completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,21 @@
+// routing libraries
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Qr from "./pages/Qr";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 export default function App() {
   return (
-    <div>App</div>
+    // routing
+    <BrowserRouter>
+      <Routes>
+        {/* page and path with each route */}
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/qr" element={<Qr />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Qr.jsx
@@ -0,0 +1,5 @@
+export default function Qr() {
+  return (
+    <div>Qr</div>
+  )
+}
