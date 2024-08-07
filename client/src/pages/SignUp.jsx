03596e982069c50f54c20c96c1d0147417d78d79
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-08-07 22:54:57 +0530

    Sign up primary completed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,12 +1,62 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import PacmanLoader from "react-spinners/PacmanLoader";
 export default function SignUp() {
+  // navigation
+  const navigation = useNavigate();
+  // states for handling data,error,loading
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  // handle changes
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData);
+  };
+  //   handle submit
+  const handleSubmit = async (e) => {
+    // here you can call your API or function to handle the form data
+    e.preventDefault();
+    // if the data filled is empty
+    if (!formData.username || !formData.email || !formData.password) {
+      return setError("Please fill all the fields");
+    }
+    // API calling and submitting
+    try {
+      // API for creating account
+      setLoading(true);
+      setError(null);
+      // res
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      //   convert the response to data
+      const data = await res.json();
+      //   if the response is ok
+      if (res.ok) {
+        //   redirect to login page
+        navigation("/sign-in");
+      }
+    } catch (error) {
+      setError("Error while creating account please try agina later....");
+      setLoading(true);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <form className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg"
+      >
         <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
         <div className="flex flex-col gap-4">
           <input
+            onChange={handleChange}
             type="text"
             name="username"
             id="username"
@@ -14,6 +64,7 @@ export default function SignUp() {
             className="border border-gray-300 p-3 w-full rounded-md focus:outline-none focus:border-blue-500"
           />
           <input
+            onChange={handleChange}
             type="email"
             name="email"
             id="email"
@@ -22,6 +73,7 @@ export default function SignUp() {
           />
           <div className="relative w-full">
             <input
+              onChange={handleChange}
               type="password"
               name="password"
               id="password"
@@ -34,7 +86,9 @@ export default function SignUp() {
           type="submit"
           className="mt-6 w-full bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
         >
-          Sign Up
+          {
+            loading?(<PacmanLoader />):("Sign Up")
+          }
         </button>
       </form>
       <div className="mt-6 text-center">
